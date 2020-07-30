db2c7a52448f1f21e6a8b207f97e40f056362785
luiz_gareis <luiz_gareis@hotmail.com>
2020-07-30 16:34:23 -0300

    Implementando controller Person.

---
+++ 03 RestWithSpringBootUdemy Scaffold Person/src/main/java/com/luizgareis/math/SimpleMath.java
@@ -0,0 +1,29 @@
+package com.luizgareis.math;
+
+public class SimpleMath {
+	
+	public Double sum(Double number1, Double number2) {
+		return number1 + number2;
+	}
+	
+	public Double substract(Double number1, Double number2) {
+		return number1 - number2;
+	}
+	
+	public Double divide(Double number1, Double number2) {
+		return number1 / number2;
+	}
+	
+	public Double multiply(Double number1, Double number2) {
+		return number1 * number2;
+	}
+	
+	public Double average(Double number1, Double number2) {
+		return (number1 + number2) / 2;
+	}
+	
+	public Double squareRoot(Double number) {
+		return Math.sqrt(number);
+	}
+
+}
---
+++ 03 RestWithSpringBootUdemy Scaffold Person/src/main/java/com/luizgareis/service/PersonService.java
@@ -0,0 +1,57 @@
+package com.luizgareis.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import com.luizgareis.models.Person;
+
+@Service
+public class PersonService {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Luiz");
+		person.setLastName("Gareis");
+		person.setGender("Male");
+		person.setAddress("Toledo, Paraná, Brasil");
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons ;
+	}
+	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setGender("Male");
+		person.setAddress("Some address in Brazil");
+		return person;
+	}
+	
+}
